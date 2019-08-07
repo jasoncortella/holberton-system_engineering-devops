9e46c61ef0cb00af0df88103da1f3a59c8c60eb9
Jason Cortella <419@holbertonschool.com>
2019-08-07 03:11:00 +0000

    Initial commit p4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# makes changes to a ssh configuration file
+
+file_line { 'Declare identity file':
+    ensure => 'present',
+      path => '/etc/ssh/ssh_config',
+      line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Turn off passwd auth':
+    ensure => 'present',
+      path => '/etc/ssh/ssh_config',
+      line => 'PasswordAuthentication no',
+    match  => 'PasswordAuthentication yes',
+}
