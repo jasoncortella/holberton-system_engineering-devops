6663ee364e1916fc603afa2178e97f9b85e6e2b7
Jason Cortella <419@holbertonschool.com>
2019-09-16 23:19:49 +0000

    Move all code into name main clause

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,13 +4,12 @@ from sys import argv
 import requests
 
 
-url = "https://jsonplaceholder.typicode.com/"
-uid = argv[1]
-user = requests.get("{}users/{}".format(url, uid)).json()
-todos = requests.get("{}todos".format(url), params={"userId": uid}).json()
-done = [task.get("title") for task in todos if task.get("completed")]
-
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    uid = argv[1]
+    user = requests.get("{}users/{}".format(url, uid)).json()
+    todos = requests.get("{}todos".format(url), params={"userId": uid}).json()
+    done = [task.get("title") for task in todos if task.get("completed")]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(done), len(todos)))
     [print("\t {}".format(title)) for title in done]
